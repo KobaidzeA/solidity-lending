7f460c40d4b825c57d0ff95befa57095af1f146b
AK <publica.k@protonmail.com>
2023-12-28 12:13:36 +0400

    changed contract address

--- src/components/SmartContract.js
+++ src/components/SmartContract.js
@@ -2,7 +2,7 @@ import Web3 from 'web3'
 import getABI from './ABI';
 
 const web3 = new Web3(window.ethereum);
-const contractAddress = "0x4242B5A3Ac2A11E4cE73273d515D4b6DeE48f074";
+const contractAddress = "0x2602E87c341C59709835F91036945d3493243159";
 var myContract = new web3.eth.Contract(getABI(),contractAddress);
 const accounts = web3.eth.getAccounts();
 
