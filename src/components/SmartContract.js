54057c4d6a062b8b84f5969396e5e41e72872087
AK <publica.k@protonmail.com>
2023-12-28 12:32:33 +0400

    potential provider fix

--- src/components/SmartContract.js
+++ src/components/SmartContract.js
@@ -1,10 +1,14 @@
 import Web3 from 'web3'
 import getABI from './ABI';
+try {
+    const web3 = new Web3(window.ethereum);
+    const contractAddress = "0x2602E87c341C59709835F91036945d3493243159";
+    var myContract = new web3.eth.Contract(getABI(),contractAddress);
+    const accounts = web3.eth.getAccounts();
+} catch(error) {
+    console.log(error);
+}
 
-const web3 = new Web3(window.ethereum);
-const contractAddress = "0x2602E87c341C59709835F91036945d3493243159";
-var myContract = new web3.eth.Contract(getABI(),contractAddress);
-const accounts = web3.eth.getAccounts();
 
 export {web3,myContract,accounts};
 
